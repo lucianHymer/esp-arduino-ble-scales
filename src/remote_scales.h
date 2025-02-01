02d5958867f0d98c2511a940dd0ba770dd9da3a4
Zach Bagnall <zach@y3m.net>
2025-02-01 15:34:00 -0700

    Update remote_scales.h

--- src/remote_scales.h
+++ src/remote_scales.h
@@ -73,11 +73,11 @@ private:
   LRUCache alreadySeenAddresses = LRUCache(100);
   std::vector<DiscoveredDevice> discoveredScales;
   void cleanupDiscoveredScales();
+  void onResult(const NimBLEAdvertisedDevice* advertisedDevice) override;
 
 public:
   std::vector<DiscoveredDevice> getDiscoveredScales() { return discoveredScales; }
 
-  void onResult(const NimBLEAdvertisedDevice* advertisedDevice) override;
   void initializeAsyncScan();
   void stopAsyncScan();
   void restartAsyncScan();
